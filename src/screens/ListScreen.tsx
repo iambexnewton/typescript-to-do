50a352559c3133a6c1089d8e7012c6fa659d1e10
Becky Newton <iambexnewton@gmail.com>
2021-10-29 13:04:53 +0200

    adding new tasks working

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,12 +1,33 @@
-import React from "react";
-
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import { nanoid } from 'nanoid'
 
 type Props= {}
 
+type Task ={id: string;
+    label: string;}
+
 const ListScreen: React.FC<Props> =()=>{
+const [tasks, setTasks] = useState<Task[]>([])
+const [newTaskLabel, setNewTaskLabel] = useState('');
+
+const handleNewTaskLabelChange = (e:ChangeEvent <HTMLInputElement>) => setNewTaskLabel(e.target.value)
+
+const handleNewTaskKeyPress = (e:KeyboardEvent <HTMLInputElement>) => {
+    if (e.key=== 'Enter' && newTaskLabel !== '') {
+         setTasks(tasks => [...tasks, {id: nanoid(), label: newTaskLabel}] )
+    setNewTaskLabel('');
+    }
+   
+
+}
+
     return(
         <div>
-            List View 
+            <ul>
+                {tasks.map((task, index) => <li key={index}>{task.label}</li>)}
+                </ul>
+            <input value={newTaskLabel} onChange={handleNewTaskLabelChange} onKeyPress={handleNewTaskKeyPress}/>
+
         </div>
     )
 }
