054a10a36dadb7153bf1fff89dbd5cf4ac4f0e04
Becky Newton <iambexnewton@gmail.com>
2021-10-29 13:48:08 +0200

    ability to clear complered tasks onclick of clear completed button

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -4,32 +4,60 @@ import { nanoid } from 'nanoid'
 type Props= {}
 
 type Task ={id: string;
-    label: string;}
+    label: string;
+isComplete: boolean}
+
+
 
 const ListScreen: React.FC<Props> =()=>{
 const [tasks, setTasks] = useState<Task[]>([])
 const [newTaskLabel, setNewTaskLabel] = useState('');
 
-const handleNewTaskLabelChange = (e:ChangeEvent <HTMLInputElement>) => setNewTaskLabel(e.target.value)
+const handleNewTaskLabelChange = (e:ChangeEvent <HTMLInputElement>) =>
+setNewTaskLabel(e.target.value)
 
 const handleNewTaskKeyPress = (e:KeyboardEvent <HTMLInputElement>) => {
     if (e.key=== 'Enter' && newTaskLabel !== '') {
-         setTasks(tasks => [...tasks, {id: nanoid(), label: newTaskLabel}] )
+         setTasks((tasks) => 
+            [
+                ...tasks,
+                 {id: nanoid(), label: newTaskLabel, isComplete: false}
+                ]);
     setNewTaskLabel('');
     }
-   
+};
 
+const handleCompleteChange =(handledTask: Task) => (e:ChangeEvent <HTMLInputElement>) => {
+    setTasks(tasks => tasks.map(task => {
+        if (task.id === handledTask.id) return {...task, isComplete: e.target.checked}
+        return task
+    }))
 }
+
+
+const handleClearClick =() => 
+setTasks((tasks)=> tasks.filter((task)=> !task.isComplete))
+
+
+
 
     return(
         <div>
-            <ul>
-                {tasks.map((task, index) => <li key={index}>{task.label}</li>)}
-                </ul>
+            <div>
+                {tasks.map((task) => (<li key={task.id}>
+                    <input type="checkbox" checked={task.isComplete} onChange={handleCompleteChange(task)}/>
+                    
+                    {task.label}</li>))}
+                </div>
             <input value={newTaskLabel} onChange={handleNewTaskLabelChange} onKeyPress={handleNewTaskKeyPress}/>
+<div>
+
+  <button onClick={handleClearClick}> Clear Completed Tasks</button>  
+</div>
 
         </div>
     )
 }
+
 
 export default ListScreen
