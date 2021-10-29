7d71cd2ba07b74db0e8a2f4faaae439a213d1803
Becky Newton <iambexnewton@gmail.com>
2021-10-29 12:26:44 +0200

    list screen working

---
+++ src/screens/ListScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+type Props= {}
+
+const ListScreen: React.FC<Props> =()=>{
+    return(
+        <div>
+            List View 
+        </div>
+    )
+}
+
+export default ListScreen
